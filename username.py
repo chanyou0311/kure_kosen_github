8805969770773b1ef2d43fd8189c51927e9bafa7
tomoohive <graphelate@gmail.com>
2018-05-04 15:14:33 +0900

    change username

--- username.py
+++ username.py
@@ -6,6 +6,6 @@ import datetime
 
 sys.stdout = codecs.getwriter('utf_8')(sys.stdout)
 
-username = 'oz2507'
+username = 'tomoohive'
 print(u'こんにちは%s。現在時刻は%sです。' % (username, datetime.datetime.now()))
 
